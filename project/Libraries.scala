0bce1bf8cbcd241f851b20100b8abdd8f64a387b
Shuwari Africa Team <developers@shuwari.africa>
2026-06-22 07:56:48 +0000

    Update boilerplate to 0.8.2

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Libraries:
   val scala3 = "org.scala-lang" %% "scala3-library" % "3.8.4"
-  val boilerplate: ModuleID = "io.github.arashi01" %% "boilerplate" % "0.8.1"
+  val boilerplate: ModuleID = "io.github.arashi01" %% "boilerplate" % "0.8.2"
   val jgit: ModuleID = "org.eclipse.jgit" % "org.eclipse.jgit" % "7.7.0.202606012155-r"
   val munit: ModuleID = "org.scalameta" %% "munit" % "1.3.3"
   val scopt: ModuleID = "com.github.scopt" %% "scopt" % "4.1.1-M3"
