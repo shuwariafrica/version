1f17d5d388868f8dd7608232028eec90ffaa9378
Ali Rashid <ali@shuwari.africa>
2026-07-06 03:52:23 +0300

    [feat] Re-enable Scala.js platform for version-core

--- project/project/BuildLibraries.scala
+++ project/project/BuildLibraries.scala
@@ -8,13 +8,10 @@ object BuildLibraries extends AutoPlugin:
   val `sbt-dynver` = sbtPlugin("com.github.sbt" % "sbt-dynver" % "5.1.1")
   val `sbt-scalafmt` = sbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.6.1")
   val `sbt-scala-native` = sbtPlugin("org.scala-native" % "sbt-scala-native" % "0.5.12")
+  val `sbt-scala-js` = sbtPlugin("org.scala-js" % "sbt-scalajs" % "1.22.0")
   val `sbt-pgp` = sbtPlugin("com.github.sbt" % "sbt-pgp" % "2.3.1")
   val `sbt-buildinfo` = sbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.13.1")
   val `sbt-header` = sbtPlugin("com.github.sbt" % "sbt-header" % "5.11.0")
-
-  // FIXME: Scala.js temporarily disabled pending sbt 2.x support - tracking scala-js/scala-js#5238.
-  // val `sbt-scalajs` = sbtPlugin("org.scala-js" % "sbt-scalajs" % scalaJSVersion)
-
   val `sbt-unidoc` = sbtPlugin("com.github.sbt" % "sbt-unidoc" % "0.6.1")
   val `sbt-mdoc` = sbtPlugin("org.scalameta" % "sbt-mdoc" % "2.9.0")
   val `sbt-bloop` = sbtPlugin("ch.epfl.scala" % "sbt-bloop" % "2.1.0")
