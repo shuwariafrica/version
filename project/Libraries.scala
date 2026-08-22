de1da25e3002bedcff9d2d57297c8e5582050f05
Shuwari Africa Team <developers@shuwari.africa>
2026-08-22 06:29:07 +0000

    [dependencies] Update boilerplate, boilerplate-effect to 0.15.0

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object Libraries:
   val scala3 = "org.scala-lang" %% "scala3-library" % "3.8.4"
-  val boilerplate: ModuleID = "africa.shuwari" %% "boilerplate" % "0.13.0"
-  val boilerplateEffect: ModuleID = "africa.shuwari" %% "boilerplate-effect" % "0.13.0"
+  val boilerplate: ModuleID = "africa.shuwari" %% "boilerplate" % "0.15.0"
+  val boilerplateEffect: ModuleID = "africa.shuwari" %% "boilerplate-effect" % "0.15.0"
   val jgit: ModuleID = "org.eclipse.jgit" % "org.eclipse.jgit" % "7.7.1.202607240634-r"
   val munit: ModuleID = "org.scalameta" %% "munit" % "1.3.5"
   val scopt: ModuleID = "com.github.scopt" %% "scopt" % "4.1.1-M3"
